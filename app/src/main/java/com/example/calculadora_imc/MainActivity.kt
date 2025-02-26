989ec988d6fd290d43046a852351e6fe1721cd01
dlilloM <dlillo_madrid@hotmail.com>
2025-02-26 10:59:27 +0100

    primera versión

--- app/src/main/java/com/example/calculadora_imc/MainActivity.kt
+++ app/src/main/java/com/example/calculadora_imc/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.example.calculadora_imc
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var heightEditText: EditText
+    lateinit var weightEditText: EditText
+    lateinit var calculateButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
+
         setContentView(R.layout.activity_main)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        heightEditText = findViewById(R.id.heightEditText)
+        weightEditText = findViewById(R.id.weightEditText)
+        calculateButton = findViewById(R.id.calculateButton)
+
+        heightEditText.setText("180")
+        weightEditText.setText("70")
+
+        calculateButton.setOnClickListener {
+            println("Altura: " + heightEditText.text.toString())
+            println("Peso: " + weightEditText.text.toString())
+        }
     }
+
 }
